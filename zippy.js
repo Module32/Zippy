9dab9cbf6298f25613f41308037df73130dd9f88
Module64 <78182297+Module32@users.noreply.github.com>
2021-12-01 20:11:01 -0500

    Create zippy.js
    It's... ALIVE!

---
+++ zippy.js
@@ -0,0 +1,29 @@
+/*
+    Zippy - get computer info, fast!
+*/
+
+const os = require('os');
+
+function zippy() {
+    console.log("Zippy's getting to work...\n")
+    try {
+        return {
+            "hostname": os.hostname(),
+            "name": os.type(),
+            "os-info": {
+                "release": os.release(),
+                "homedir": JSON.stringify(os.cpus()),
+                "freememory": os.freemem(),
+                "loadavg": os.loadavg(),
+            },
+            "cpu-info": {
+                "arch": os.arch(),
+                "cpus": JSON.stringify(os.cpus()),
+            },
+        }
+    } catch (err) {
+        return console.log(`Something went wrong! Error:\n${err.stack}`)
+    }
+}
+
+console.log(zippy())
